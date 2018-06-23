e2e4cd9e5bcc80540e311617732101f5b4d5bc78
dpdom <domenicodipietroing@gmail.com>
2018-06-23 17:17:37 +0200

    fixed: Service Worker: fixed registration issue

--- js/main.js
+++ js/main.js
@@ -4,24 +4,6 @@ let restaurants,
 var newMap
 var markers = []
 
-
-/**
- * Service Worker *
- */
-
- // See: chrome://inspect/#service-workers
- if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js', {scope: './'}).then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
